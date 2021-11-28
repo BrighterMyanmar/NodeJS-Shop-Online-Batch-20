f5e5b3af45b6b40282cf730bd2b8503a7a624317
Brighter Myanmar <brightermyanmar@gmail.com>
2021-11-28 17:34:58 +0630

    Project Done

--- utils/chat.js
+++ utils/chat.js
@@ -14,6 +14,16 @@ let initialize = async (io, socket) => {
 
    socket.on('unread', data => sendUnreadMsg(socket));
    socket.on('message', data => incommingMessage(io, socket, data));
+   socket.on('load-more', data => loadMore(socket, data));
+}
+
+let loadMore = async (socket, data) => {
+   let limit = Number(process.env.MSG_LIMIT);
+   let skip = data.page == 0 ? 0 : Number(data.page) * limit;
+   let messages = await MessageDB.find({
+      $or: [{ from: socket.currentUserId }, { to: socket.currentUserId }]
+   }).sort({ created: -1 }).skip(skip).limit(limit).populate('from to', 'name _id');
+   socket.emit('messages', messages);
 }
 
 let incommingMessage = async (io, socket, message) => {
@@ -23,22 +33,26 @@ let incommingMessage = async (io, socket, message) => {
    let toUser = await Redis.getObj(message.to);
 
    if (toUser) {
-      let toSocket = io.of("/chat").connected;
-      console.log(toSocket);
-      // if (toSocket) {
-      //    toSocket.emit('message', msgResult);
-      // }
+      // let toSocket = io.of("/chat").connected[toUser.socketId]; // V4
+      let toSocket = io.of('/chat').sockets.get(toUser.socketId);  // V2
+      if (toSocket) {
+         console.log("User is online");
+         toSocket.emit('message', msgResult);
+      } else {
+         console.log("User is offline");
+         await new UnreadDB({ from: message.from, to: message.to }).save();
+      }
    } else {
       await new UnreadDB({ from: message.from, to: message.to }).save();
    }
-
 }
 
 let sendUnreadMsg = async (socket) => {
    let unreads = await UnreadDB.find({ to: socket.currentUserId });
+   console.log(unreads.length);
    if (unreads.length > 0) {
       for (let unread of unreads) {
-         await UnreadDB.findByIdAndDelete(unrad._id);
+         await UnreadDB.findByIdAndDelete(unread._id);
       }
    }
    socket.emit('unread', { msg: unreads.length });
